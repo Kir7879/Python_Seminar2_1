710eaf9190075cf8a4800da59c17d16d052a5092
Kir7879 <122403301+Kir7879@users.noreply.github.com>
2023-07-24 19:19:58 +0300

    Add files via upload

---
+++ seminar2_3.py
@@ -0,0 +1,11 @@
+print('Введите любое целое число больше 0')
+N = int(input('N='))
+count = 1
+i = 0
+# for i in range(N):
+#     print(N)
+while count <= N:
+    print(count, end =  ' ')
+    count = count * 2
+    i = i + 1
+   
